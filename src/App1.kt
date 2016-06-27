17daea781c23ae811da692ce4a68138c8c1925c1
Philip Wilcox <philipwilcox@gmail.com>
2016-06-26 21:32:46 -0700

    style and basic functionality of rolling multiple dice

--- src/App1.kt
+++ src/App1.kt
@@ -2,8 +2,13 @@ import java.security.SecureRandom
 
 fun main(args: Array<String>) {
     println("Hello, world!")
-    val one_die = Die()
-    println("First roll: ${one_die.roll()}; second roll: ${one_die.roll()}")
+    val oneDie = Die()
+    println("First roll: ${oneDie.roll()}; second roll: ${oneDie.roll()}")
+    val attackDice = AttackDice(3)
+    for (i in 1..10) {
+        val results = attackDice.rollDice()
+        println("Result of rolling attack dice: ${results}")
+    }
 }
 
 class Die {
@@ -12,32 +17,31 @@ class Die {
     fun roll() = random.nextInt(6) + 1
 }
 
-open class Dice(dice_count: Int) {
-    val dice_count = dice_count
+open class Dice(diceCount: Int) {
+    val dice = (1..diceCount).map { Die() }
 
-    fun roll_dice() = {
-        // TODO Jun 26, pmw: roll these dice, return sorted descending-order list of the rolls
+    /**
+     * Roll this set of dice, returning the value as a list of sorted (largest to smallest) integers.
+     */
+    fun rollDice(): List<Int> {
+        return dice.map { d -> d.roll() }.sortedDescending()
     }
 }
 
-class AttackDice(dice_count: Int) : Dice(dice_count) {
+class AttackDice(diceCount: Int) : Dice(diceCount) {
     init {
-        if (dice_count < 2)
+        if (diceCount < 2)
             throw Exception("Must attack with at least 2 dice")
-        if (dice_count > 3)
+        if (diceCount > 3)
             throw Exception("Cannot attack with more than 3 dice")
     }
 }
 
-class DefendDice(dice_count: Int) : Dice(dice_count) {
+class DefendDice(diceCount: Int) : Dice(diceCount) {
     init {
-        if (dice_count < 1)
+        if (diceCount < 1)
             throw Exception("Must defend with at least 1 die")
-        if (dice_count > 2)
+        if (diceCount > 2)
             throw Exception("Cannot defend with more than 2 dice")
     }
-}
-
-fun roll_attack(attack_dice: Int, defend_dice: Int) {
-
 }
